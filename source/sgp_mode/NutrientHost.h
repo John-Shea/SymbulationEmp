22b03b38f6bcbe9b1c23d45290bac41c972f24f3
John Shea <sheajo@mail.gvsu.edu>
2025-03-24 11:42:50 -0400

    implmented framework for NutrientHost.h

--- source/sgp_mode/NutrientHost.h
+++ source/sgp_mode/NutrientHost.h
@@ -45,76 +45,37 @@ public:
     return host_baby;
   }
 
-
-  void Process(emp::WorldPosition pos){
-    if (GetDead()) {
-      return; // If the host is dead, no further processing.
-    }
-
-    bool host_cycle = true;
-    if (HasSym()) {
-      host_cycle = random->P(0.5); 
-    }
-
-    // If the host is processing, run the host's CPU step.
-    if (host_cycle) {
-      GetCPU().RunCPUStep(pos, sgp_config->CYCLES_PER_UPDATE());
-    }
-
-    // Process symbionts if the host isn't processing.
-    if (HasSym() && !host_cycle) {
-      emp::vector<emp::Ptr<Organism>> &syms = GetSymbionts();
-      for (size_t j = 0; j < syms.size(); j++) {
-        emp::Ptr<Organism> curSym = syms[j];
+  // Process function that handles the host and symbionts.
+  void Process(emp::WorldPosition pos) {
+    if (my_world->GetUpdate() % sgp_config->EXTINCTION_FREQUENCY() == 0) {
+      double death_chance = sgp_config->BASE_DEATH_CHANCE();
         if (GetDead()) {
-          return; // Stop processing if the host is dead.
+            return; // If the host is dead, no further processing.
         }
-
-        // Process the symbiont if it's not dead.
-        emp::WorldPosition sym_pos = emp::WorldPosition(j + 1, pos.GetIndex());
-        if (!curSym->GetDead()) {
-          curSym->Process(sym_pos); // Call the symbiont's Process method.
-
-            // TODO: ask 
-          // Check if the symbiont performed a task that matches the host's recent task.
-          if (SymbiontPerformedTask(curSym)) {
-            // If the operation matches the host's recent operation, steal resources.
-            double resources_stolen = StealResources(curSym->GetIntVal());
-            if (resources_stolen > 0) {
-
-                /// ask about infinity resources 
-              // Steal resources from the host and the world. The resources are split.
-              double world_resources = my_world->PullResources(resources_stolen);
-              double stolen_from_host = resources_stolen / 2;
-              double stolen_from_world = resources_stolen / 2;
-              this->AddPoints(stolen_from_world); 
-              curSym->AddPoints(stolen_from_host); 
-            }
-          } else {
-            // If the operation doesn't match the host's recent operation, distribute resources equally.
-            double resources = curSym->GetIntVal(); // Symbiont earns resources.
-            double half = resources / 2;
-            this->AddPoints(half); // Half goes to the host.
-            curSym->AddPoints(half); // Half goes to the symbiont.
-          }
-        }
-
-        // If the symbiont dies during processing, remove it from the list.
-        if (curSym->GetDead()) {
-          syms.erase(syms.begin() + j);
-          curSym.Delete();
-        }
-      } // For each sym in syms
-    } // If the host has symbionts
-  }
-  // TODO: Helper function to check if the symbiont performed a task that matches the host's recent task
-  bool SymbiontPerformedTask(emp::Ptr<Organism> curSym) {
-
-
-    return false; 
-  }
+        GetCPU().RunCPUStep(pos, sgp_config->CYCLES_PER_UPDATE());
+        emp::vector<emp::Ptr<Organism>> &hosts = GetHost();
+        for (size_t i = 0; i < hosts.size(); i++) {
+            if (HasSym() & hosts->IsHost()) {
+                if (sgp_config->NUTRIENT_TYPE() == MUTUALIST) death_chance = sgp_config->MUTUALIST_DEATH_CHANCE();
+                else if (sgp_config->NUTRIENT_TYPE() == PARASITE) death_chance = sgp_config->PARASITE_DEATH_CHANCE();
+                emp::vector<emp::Ptr<Organism>> &syms = GetSymbionts();
+                for (size_t j = 0; j < syms.size(); j++) {
+                    emp::Ptr<Organism> curSym = syms[j];
+                    if (GetDead()) {
+                        return;
+                    }
+                    emp::WorldPosition sym_pos = emp::WorldPosition(j + 1, pos.GetIndex());
+                    if (!curSym->GetDead()) {
+                        curSym->Process(sym_pos); // Call the symbiont's Process method.
+                        // TODO: probably need to clean up host death too
+                        if (curSym->GetDead()) {
+                            syms.erase(syms.begin() + j);
+                            curSym.Delete();
+                         }
+                    } 
+                } 
+                } 
+        } 
+    } 
 };
-
-}
-
-#endif
+#endif // NUTRIENTHOST_H
